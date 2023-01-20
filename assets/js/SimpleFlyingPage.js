eb60a6f45ddc7a47e4a98c02499d23f9a6bbea51
kobe-koto <91669733+kobe-koto@users.noreply.github.com>
2023-01-20 19:33:05 +0800

    没有优化和改进.

--- assets/js/SimpleFlyingPage.js
+++ assets/js/SimpleFlyingPage.js
@@ -2,27 +2,46 @@
  * a simple flying page javascript just for same origin
  */
 
-window.location.flying = async function (TargetURL) {
-    let TargetURLObject =
-        (function () {
-            try {
-                return new URL(TargetURL)
-            } catch (e) {
-                return "notValid"
+if (
+    document.querySelectorAll &&
+    window.fetch && window.DOMParser
+) {
+    function DOMLoadedEval () {
+        let Links2Fly = document.querySelectorAll("a[href]");
+        for (var i=0; i<Links2Fly.length; i++) {
+            let link = toURLObj(Links2Fly[i].href);
+            if (link === "notValid" || link.hostname === location.hostname) {
+                Links2Fly[i].href = "javascript:location.flying('" + Links2Fly[i].href + "')"
             }
-        })();
+        }
+    }
+    addEventListener("DOMContentLoaded", DOMLoadedEval)
+}
+
+window.location.flying = async function (TargetURL) {
+    var TargetURLObject = toURLObj(TargetURL);
 
     if (
         (TargetURLObject === "notValid" || TargetURLObject.hostname === location.hostname) &&
-        window.fetch
+        window.fetch && window.DOMParser
     ) {
-        let TargetResponse = await fetch(TargetURL).then(res => res.text())
-        let TempParser = new DOMParser();
-        let TargetDOM = TempParser.parseFromString(TargetResponse, "text/html");
-        document.title = TargetDOM.title;
-        document.body.innerHTML = TargetDOM.body.innerHTML;
-        history.pushState({}, "0", TargetURL)
+            var TargetResponse = await fetch(TargetURL).then(function (res) {return res.text()})
+            var TempParser = new DOMParser();
+            var TargetDOM = TempParser.parseFromString(TargetResponse, "text/html");
+            document.title = TargetDOM.title;
+            document.body.innerHTML = TargetDOM.body.innerHTML;
+            history.pushState({}, "0", TargetURL)
+            DOMLoadedEval()
     } else {
         location.assign(TargetURL);
     }
 }
+
+function toURLObj (Strings) {
+    try {
+        return new URL(Strings)
+    } catch (e) {
+        return "notValid"
+    }
+}
+
